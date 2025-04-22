b66b169a9d82a487fd432714acb98309c0929078
AlexNg <contact@ngjx.org>
2025-04-22 16:43:27 +0800

    fix: Bind to 0.0.0.0
    Signed-off-by: AlexNg <contact@ngjx.org>

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ async fn main() {
                 }),
             );
 
-    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
